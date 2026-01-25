6f40e0b38819e70cba7527c983530e6a55102bb5
Chris Huber <chubes4@users.noreply.github.com>
2026-01-25 11:42:54 -0600

    release: v0.2.6

--- extrachill-search.php
+++ extrachill-search.php
@@ -3,7 +3,7 @@
  * Plugin Name: ExtraChill Search
  * Plugin URI: https://extrachill.com
  * Description: Network-wide search across all nine sites using domain-based resolution
- * Version: 0.2.5
+ * Version: 0.2.6
  * Author: Chris Huber
  * Author URI: https://chubes.net
  * Network: true
