de6f1f6bed540837deb329b2a4610957e4624aa2
Chris Huber <chubes@extrachill.com>
2026-08-23 19:04:50 +0000

    test: complete search request fixture

--- tests/fixtures/SearchRequestFunctions.php
+++ tests/fixtures/SearchRequestFunctions.php
@@ -13,6 +13,14 @@ function wp_unslash( $value ) {
 	return $value;
 }
 
+function wp_parse_url( $url, $component = -1 ) {
+	return parse_url( $url, $component );
+}
+
+function wp_strip_all_tags( $text ) {
+	return strip_tags( $text );
+}
+
 function get_search_query() {
 	global $query_vars;
 	return $query_vars['s'] ?? '';
